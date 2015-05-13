81df91dbea47cf9deb97343e7005523e402f4890
Henry <omgredstone@gmail.com>
2015-05-13 10:29:20 +0100

    started working on stuff

--- js/main.js
+++ js/main.js
@@ -4,15 +4,30 @@ var defaults = {
     "LOADING_GIF": {
       "24": "http://s.imgur.com/images/loaders/ddddd1_181817/24.gif",
       "48": "http://s.imgur.com/images/loaders/ddddd1_181817/48.gif"
-    }
+    },
+    "CUSTOM_CSS": ""
+  },
+  "CUSTOMIZATION": {
+    ""
   }
 }
 
 var settings = {};
 
-function loadUserSettings(user) {
+function loadSettingsFromCookies() {
   settings.SETTINGS.LOADING_GIF["24"] = getCookie("0x211") | defaults.SETTINGS.LOADING_GIF["24"];
   settings.SETTINGS.LOADING_GIF["48"] = getCookie("0x212") | defaults.SETTINGS.LOADING_GIF["48"];
+  settings.SETTINGS.CUSTOM_CSS = getCookie("0x121") | defaults.SETTINGS.CUSTOM_CSS;
+}
+
+function applySettings() {
+  if (settings.SETTINGS.LOADING_GIF["24"]) {
+    
+  }
+}
+
+function applyLoader(url) {
+  
 }
 
 function getCookie(id) {
